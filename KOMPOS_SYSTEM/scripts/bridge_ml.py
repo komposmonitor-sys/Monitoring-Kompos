b231b1dcdd4d66ba7bf4c8beac25a8bc0f979fa3
MuhaammadSyaban28 <syaban28@upi.edu>
2026-01-02 15:04:52 +0700

    Update bridge_ml.py

--- KOMPOS_SYSTEM/scripts/bridge_ml.py
+++ KOMPOS_SYSTEM/scripts/bridge_ml.py
@@ -26,3 +26,29 @@ def trimf(x, params):
     if a < x <= b: return (x - a) / (b - a)
     if b < x < c: return (c - x) / (c - b)
     return 0.0
+
+def hitung_membership(suhu, moisture, ph, ammonia, bau_val):
+    """Menghitung derajat keanggotaan (Fuzzification)."""
+    mu = {}
+
+    # --- SUHU ---
+    mu['suhu_dingin'] = trapmf(suhu, [0, 0, 28, 35]) 
+    mu['suhu_ideal']  = trimf(suhu, [30, 45, 55])
+    mu['suhu_panas']  = trapmf(suhu, [50, 60, 80, 80])
+
+    # --- KELEMBAPAN (MOISTURE) ---
+    mu['kelembapan_kering'] = trapmf(moisture, [0, 0, 30, 40])
+    mu['kelembapan_sedang'] = trimf(moisture, [40, 46, 52]) 
+    mu['kelembapan_basah']  = trapmf(moisture, [50, 60, 100, 100])
+
+    # --- PH ---
+    mu['ph_asam']   = trapmf(ph, [0, 0, 5, 6])
+    mu['ph_netral'] = trimf(ph, [5.0, 7.0, 9.0])
+    mu['ph_basa']   = trapmf(ph, [8, 9, 14, 14])
+
+    # --- VARIABEL SAFETY (AMMONIA & BAU) ---
+    mu['ammo_tinggi']   = trapmf(ammonia, [25, 30, 50, 50])
+    mu['bau_menyengat'] = trapmf(bau_val, [6, 8, 10, 10])
+
+    return mu
+    
